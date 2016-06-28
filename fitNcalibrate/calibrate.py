5472958647d87eed542452ebe3bb3cdbac76aeb7
ksbeerna <kelly.beernaert@cern.ch>
2016-06-28 16:54:22 +0200

    * example solutions for setting up the pseudo-experiments and calibration curve

---
+++ fitNcalibrate/calibrate.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python                                                                                                                                                                                       
+ 
+import math, ROOT, json, optparse, os, sys, pprint, array
+from ROOT import TCanvas, TGraphErrors, TLine, TF1
+from array import array
+from math import *
+
+def calibrate(offset=None,slope=None,offset_err=None,slope_err=None,meas=None):
+    
+    #first get calibrated Eb from the calibration curve and the measured value
+    calibrated = (meas - offset)/slope + 67.57
+    m_W = 80.385
+    m_b = 4.18
+    m_t = calibrated + sqrt(m_W*m_W - m_b*m_b + calibrated*calibrated)
+    return m_t
+
+def setupCalibrationCurve():
+    #input Eb based on input top mass of 169.5 GeV, 171.5 GeV, 172.5 GeV, 173.5 GeV, 175.5 GeV 
+    Eb_input = array('f', [65.74, 67.57, 69.39])
+    Eb_input_err = array('f', [0.0, 0.0, 0.0])
+    #average Eb as output from the pseudo-experiments
+    Eb_output = array('f', [64.74, 66.81, 67.85])
+    #average uncertainty on Eb as output from the pseudo-experiments
+    Eb_output_err = array('f', [0.69, 0.76, 0.74])
+    #Eb_output_err = array('f', [0.02, 0.02, 0.02])
+
+    graph = TGraphErrors(len(Eb_input), Eb_input, Eb_output, Eb_input_err, Eb_output_err)
+    graph.SetMarkerStyle(8)
+    graph.SetMarkerSize(0.7)
+
+    fitFunction = TF1("f1", "[0] + [1]*(x-67.57)", 64, 70)
+#    fitFunction = TF1("f1", "[0] + [1]*(x)", 64, 70)
+    fitFunction.SetLineColor(3)
+    fitRes = graph.Fit("f1","S")
+    offset = fitRes.Parameter(0)
+    offset_err = fitRes.ParError(0)
+    slope = fitRes.Parameter(1)
+    slope_err = fitRes.ParError(1)
+    chi = fitRes.Chi2()
+    cor = fitRes.Correlation(0,1)
+
+    print "offset = (%3.2f #pm %3.2f)" % (offset, offset_err)
+    print "slope = (%3.2f #pm %3.2f)" % (slope, slope_err)
+    print "chi2 = %3.2f" % chi
+    print "correlation = %3.2f" % cor
+
+    c1 = TCanvas("c1", "c1",0,0,600,500);
+    c1.Range(0,0,1,1)
+    h2 = c1.DrawFrame(64,64,70,70)
+    h2.GetXaxis().SetTitle("Eb_{input}")
+    h2.GetXaxis().SetTitleSize(0.06)
+    h2.GetXaxis().SetLabelSize(0.06)
+    h2.GetXaxis().SetTitleOffset(0.45)
+    h2.GetYaxis().SetTitle("Eb_{output}")
+    h2.GetYaxis().SetTitleSize(0.06)
+    h2.GetYaxis().SetLabelSize(0.06)
+    h2.GetYaxis().SetTitleOffset(0.45)
+    graph.Draw("p")
+    line = TLine(64,64,70,70)
+    line.Draw("same")
+    fitFunction.Draw("same")
+    c1.Modified()
+    c1.Update()
+    c1.SaveAs("calibrationCurve.pdf")
+
+    return offset, slope, offset_err, slope_err
+
+def main():
+
+    offset,slope,offset_err,slope_err = setupCalibrationCurve()
+    calibrate(offset,slope,offset_err,slope_err, x)
+
+if __name__ == "__main__":
+    sys.exit(main())
+
--- fitNcalibrate/fitPeak.py
+++ fitNcalibrate/fitPeak.py
@@ -31,8 +31,10 @@ def gPeak(h=None,inDir=None,isData=None,lumi=None):
     ## Set the function
     fitfunc = TF1("Gaussian fit", myFitFunc, minToFit, maxToFit, 3)
     ## Set normalization
-    fitfunc.SetParameter(2, h.Integral());
-    fitfunc.SetParLimits(2, 0.1*h.Integral(), 2.5*h.Integral());
+    #fitfunc.SetParameter(2, h.Integral());
+    #fitfunc.SetParLimits(2, 0.1*h.Integral(), 2.5*h.Integral());
+    fitfunc.SetParameter(0, h.Integral());
+    fitfunc.SetParLimits(0, 0.1*h.Integral(), 2.5*h.Integral());
     ## Set gaussian mean starting value and limits
     fitfunc.SetParameter(1, 4.2);
     fitfunc.SetParLimits(1, 4., 4.4);
@@ -45,7 +47,7 @@ def gPeak(h=None,inDir=None,isData=None,lumi=None):
     fitfunc.SetLineStyle(1)
 
     # Do the fit
-    hFit.Fit("Gaussian fit","EM", "", minToFit, maxToFit) 
+    hFit.Fit("Gaussian fit","EMQ", "", minToFit, maxToFit) 
     # "E" stands for Minos, "M" for improving fit results
     # cf. ftp://root.cern.ch/root/doc/5FittingHistograms.pdf    
 
@@ -90,81 +92,81 @@ def gPeak(h=None,inDir=None,isData=None,lumi=None):
 
     # Plot the result
     ## Create a canvas with two pads for plotting your histograms
-    c = TCanvas('c','c')
-    p1 = ROOT.TPad('p1','p1',0.,0.3,1.0,1.0)
-    p1.SetBorderMode(0)
-    p1.SetBorderSize(2)
-    p1.SetTickx(1)
-    p1.SetTicky(1)
-    p1.SetTopMargin(0.13)
-    p1.SetBottomMargin(0.02)
-    p1.Draw()    
-    p2 = ROOT.TPad('p2','p2',0.,0.,1.0,0.3)
-    p2.SetGridy()
-    p2.SetBorderMode(0)
-    p2.SetBorderSize(2)
-    p2.SetTickx(1)
-    p2.SetTicky(1)
-    p2.SetTopMargin(0.05)
-    p2.SetBottomMargin(0.3)
-    p2.Draw()
+    #c = TCanvas('c','c')
+    #p1 = ROOT.TPad('p1','p1',0.,0.3,1.0,1.0)
+    #p1.SetBorderMode(0)
+    #p1.SetBorderSize(2)
+    #p1.SetTickx(1)
+    #p1.SetTicky(1)
+    #p1.SetTopMargin(0.13)
+    #p1.SetBottomMargin(0.02)
+    #p1.Draw()    
+    #p2 = ROOT.TPad('p2','p2',0.,0.,1.0,0.3)
+    #p2.SetGridy()
+    #p2.SetBorderMode(0)
+    #p2.SetBorderSize(2)
+    #p2.SetTickx(1)
+    #p2.SetTicky(1)
+    #p2.SetTopMargin(0.05)
+    #p2.SetBottomMargin(0.3)
+    #p2.Draw()
     ## Draw in the pad of the fit
-    p1.cd()
-    hFit.GetXaxis().SetRangeUser(minToFit,maxToFit)     
-    hFit.Draw()
+    #p1.cd()
+    #hFit.GetXaxis().SetRangeUser(minToFit,maxToFit)     
+    #hFit.Draw()
     ##Create some labels about the statistics
-    caption1 = TLatex()
-    caption1.SetTextSize(0.045)
-    caption1.SetTextFont(42)
-    caption1.SetNDC()
-    caption1.DrawLatex(0.75,0.8,'Fit Results')
-    caption1.DrawLatex(0.73,0.76,'#mu = %4.2f #pm %4.2f'%(mean,meanErr))
-    caption1.DrawLatex(0.73,0.72,'#sigma = %4.2f #pm %4.2f'%(sigma,sigmaErr))
-    caption1.DrawLatex(0.74,0.67,'#chi^{2}/ndf = %4.2f'%(chi2ndf))
-    caption2 = TLatex()
-    caption2.SetTextSize(0.05)
-    caption2.SetTextFont(42)
-    caption2.SetNDC()  
-    caption2.DrawLatex(0.35,0.44,'Uncalibrated Measurement')
-    caption2.DrawLatex(0.35,0.39,'<E_{b}> = (%4.2f #pm %4.2f) GeV'%(Ereco,Err))
+    #caption1 = TLatex()
+    #caption1.SetTextSize(0.045)
+    #caption1.SetTextFont(42)
+    #caption1.SetNDC()
+    #caption1.DrawLatex(0.75,0.8,'Fit Results')
+    #caption1.DrawLatex(0.73,0.76,'#mu = %4.2f #pm %4.2f'%(mean,meanErr))
+    #caption1.DrawLatex(0.73,0.72,'#sigma = %4.2f #pm %4.2f'%(sigma,sigmaErr))
+    #caption1.DrawLatex(0.74,0.67,'#chi^{2}/ndf = %4.2f'%(chi2ndf))
+    #caption2 = TLatex()
+    #caption2.SetTextSize(0.05)
+    #caption2.SetTextFont(42)
+    #caption2.SetNDC()  
+    #caption2.DrawLatex(0.35,0.44,'Uncalibrated Measurement')
+    #caption2.DrawLatex(0.35,0.39,'<E_{b}> = (%4.2f #pm %4.2f) GeV'%(Ereco,Err))
     ## CMS labels
-    label1 = TLatex()
-    label1.SetNDC()
-    label1.SetTextFont(60)
-    label1.SetTextSize(0.09)
-    label1.SetTextAlign(31)
-    label1.DrawLatex(0.19, 0.9, "CMS")
-    label2 = TLatex()
-    label2.SetNDC()
-    label2.SetTextFont(42)
-    label2.SetTextSize(0.0765)
-    label2.SetTextAlign(11)
-    if isData is True:
-        label2.DrawLatex(0.2, 0.9, "#it{Preliminary}")
-    else:
-        label2.DrawLatex(0.2, 0.9, "#it{Simulation preliminary}")
-    label3 = TLatex()
-    label3.SetNDC()
-    label3.SetTextFont(42)
-    label3.SetTextSize(0.0765)
-    label3.SetTextAlign(31)
-    label3.DrawLatex(0.90, 0.9, "%d pb^{-1} (13 TeV)" % lumi)
+    #label1 = TLatex()
+    #label1.SetNDC()
+    #label1.SetTextFont(60)
+    #label1.SetTextSize(0.09)
+    #label1.SetTextAlign(31)
+    #label1.DrawLatex(0.19, 0.9, "CMS")
+    #label2 = TLatex()
+    #label2.SetNDC()
+    #label2.SetTextFont(42)
+    #label2.SetTextSize(0.0765)
+    #label2.SetTextAlign(11)
+    #if isData is True:
+    #    label2.DrawLatex(0.2, 0.9, "#it{Preliminary}")
+    #else:
+    #    label2.DrawLatex(0.2, 0.9, "#it{Simulation preliminary}")
+    #label3 = TLatex()
+    #label3.SetNDC()
+    #label3.SetTextFont(42)
+    #label3.SetTextSize(0.0765)
+    #label3.SetTextAlign(31)
+    #label3.DrawLatex(0.90, 0.9, "%d pb^{-1} (13 TeV)" % lumi)
     ## Edit the pad for the pull
-    p2.cd()
-    hPull.GetXaxis().SetRangeUser(minToFit,maxToFit)
-    hPull.Draw("e")
+    #p2.cd()
+    #hPull.GetXaxis().SetRangeUser(minToFit,maxToFit)
+    #hPull.Draw("e")
 
-    #save and delete
-    sName = inDir+"/fit_";
-    if isData is True:
-        sName = sName+"Data";
-    else: 
-        sName = sName+"MC";
-    c.SaveAs(sName+".pdf");
-    c.SaveAs(sName+".png");
-    del c
-    fitfunc.IsA().Destructor(fitfunc)
-    del caption1,caption2
+    ##save and delete
+    #sName = inDir+"/fit_";
+    #if isData is True:
+    #    sName = sName+"Data";
+    #else: 
+    #    sName = sName+"MC";
+    #c.SaveAs(sName+".pdf");
+    #c.SaveAs(sName+".png");
+    #del c
+    #fitfunc.IsA().Destructor(fitfunc)
+    #del caption1,caption2
 
     #all done here ;)
     return Ereco,Err
@@ -214,9 +216,91 @@ def main():
            if not os.path.isdir(opt.inDir):
                os.mkdir(opt.inDir)
 
+           # Draw pseudo-experiments for calibration (sample size is observed dataset size, hardcoded for now)
+           #dataSize = 14760
+           Eb_histo = TH1F("Eb_histo","",100,62,72)
+           DEb_histo = TH1F("DEb_histo","",100,0.2,1.2)
+           if opt.isData is not True:
+               random3 = TRandom3()
+               # Run 100 pseudo-experiments
+               for i in range(0, 1000):
+                   temp = TH1F("temp", "", 20, 3, 7)
+
+                   # Run over dataset size
+                   dataSize = random3.Poisson(14760)
+                   for j in range(0, dataSize):
+                       temp.Fill(histo.GetRandom())
+
+                   Eb_temp,DEb_temp = gPeak(h=temp,inDir=opt.inDir,isData=opt.isData,lumi=opt.lumi)
+                   print "iteration %i dataset size %i   <E_{b}> = (%3.2f #pm %3.2f) GeV" % (i, dataSize ,Eb_temp,DEb_temp)
+                   Eb_histo.Fill(Eb_temp)
+                   DEb_histo.Fill(DEb_temp)
+                   del temp
+               print "finished pseudo-experiments"
+
+               #fit the results with a Gaussian
+               Eb_fitfunc = TF1("Eb Gaussian fit", myFitFunc, 62, 72, 3)
+               Eb_fitfunc.SetParameter(0, 1000);
+               #Eb_fitfunc.SetParLimits(0, 0.1*1000, 2.5*1000);
+               Eb_fitfunc.SetParameter(1, 67);
+               #Eb_fitfunc.SetParLimits(1, 4., 4.4);
+               Eb_fitfunc.SetParameter(2, 1);
+               #Eb_fitfunc.SetParLimits(2, 0.35, 0.95);
+               Eb_fitfunc.SetLineColor(kBlue)
+               Eb_fitfunc.SetLineWidth(3)
+               Eb_fitfunc.SetLineStyle(1)
+               Eb_histo.Fit("Eb Gaussian fit","EMQ", "", 62, 72)           
+               mean_Eb = Eb_fitfunc.GetParameter(1)
+               err_mean_Eb = Eb_fitfunc.GetParError(1)
+
+               
+               DEb_fitfunc = TF1("DEb Gaussian fit", myFitFunc, 0.2, 1.2, 3)
+               DEb_fitfunc.SetParameter(0, 1000);
+               #DEb_fitfunc.SetParLimits(0, 0.1*1000, 2.5*1000);
+               DEb_fitfunc.SetParameter(1, 0.8);
+               #DEb_fitfunc.SetParLimits(1, 4., 4.4);
+               DEb_fitfunc.SetParameter(2, 0.2);
+               #DEb_fitfunc.SetParLimits(2, 0.35, 0.95);
+               DEb_fitfunc.SetLineColor(kBlue)
+               DEb_fitfunc.SetLineWidth(3)
+               DEb_fitfunc.SetLineStyle(1)
+               DEb_histo.Fit("DEb Gaussian fit","EMQ", "", 0.2, 1.2)           
+               mean_DEb = DEb_fitfunc.GetParameter(1)
+               err_mean_DEb = DEb_fitfunc.GetParError(1)
+
+               print "PE <E_{b}> = (%3.2f #pm %3.2f) GeV" % (mean_Eb,err_mean_Eb)
+               print "PE <#delta E_{b}> = (%3.4f #pm %3.4f) GeV" % (mean_DEb,err_mean_DEb)
+
+               #draw results of pseudo-experiments
+               cEb = TCanvas("PE canvas 1", "c1", 600, 600)
+               cEb.cd()
+               Eb_histo.Draw("ehist")
+               Eb_fitfunc.Draw("same")
+               cEb.SaveAs("PE_Eb.pdf")
+               cDEb = TCanvas("PE canvas 2", "c1", 600, 600)
+               cDEb.cd()
+               DEb_histo.Draw("ehist")
+               DEb_fitfunc.Draw("same")
+               cDEb.SaveAs("PE_DEb.pdf")
+
+
+
            # Calculate the energy peak position in the big MC sample
            Eb,DEb = gPeak(h=histo,inDir=opt.inDir,isData=opt.isData,lumi=opt.lumi)
            print "<E_{b}> = (%3.2f #pm %3.2f) GeV" % (Eb,DEb)
+
+           # uncalibrated top quark mass calculation
+           m_w = 80.385
+           m_w_err = 0.015
+           m_b = 4.18
+           m_b_err = 0.03
+           temp = math.sqrt(m_w*m_w - m_b*m_b + Eb*Eb)
+           m_top = Eb + temp
+           temp2 = (1 + Eb/temp)*(1 + Eb/temp)*DEb*DEb + (m_w/temp)*(m_w/temp)*m_w_err*m_w_err + (m_b/temp)*(m_b/temp)*m_b_err*m_b_err
+           m_top_err = math.sqrt(temp2)
+           print "uncalibrated m_top = (%3.2f #pm %3.2f ) GeV" % (m_top,m_top_err)
+
+
 
            res.Close()
                
